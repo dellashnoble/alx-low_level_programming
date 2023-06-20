526618afded0a04769455c871ebcbb11db40b1b6
Otaru Favour <ashfabotaru@gmail.com>
2023-06-20 12:47:51 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+/**
+ * _abs - Computes the absolute value of an integer
+ *
+ * @n: The integer to compute the absolute value
+ *
+ * Return: The absolute value of @n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
