b8e9197220932061204a2ec30a0b7d5801c91e4e
Otaru Favour <ashfabotaru@gmail.com>
2023-06-20 12:11:43 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase, ten times
+ *
+ * Description: Prints the lowercase alphabet ten times, followed by a new line
+ *              Uses the _putchar function to print characters.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	}
+
+	_putchar('\n');
+}
+
