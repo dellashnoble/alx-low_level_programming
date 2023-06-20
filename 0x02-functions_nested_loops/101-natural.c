3487b0c1369afd74bf227c833baa8f3e046f49d8
Otaru Favour <ashfabotaru@gmail.com>
2023-06-20 14:39:02 -0700

    task13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,46 @@
 #include "main.h"
 
 /**
+ * print_sum - Prints the sum of multiples of 3 or 5 below 1024
+ *
+ * @sum: The sum to be printed
+ */
+void print_sum(int sum)
+{
+	if (sum == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		int digits = 0;
+		int temp = sum;
+
+		/* Count the number of digits in the sum */
+		while (temp != 0)
+		{
+			digits++;
+			temp /= 10;
+		}
+
+		/* Extract and print each digit */
+		while (digits > 0)
+		{
+			int divisor = 1;
+			int j;
+
+		/* Calculate the divisor based on the number of digits */
+			for (j = 1; j < digits; j++)
+				divisor *= 10;
+
+			_putchar((sum / divisor) + '0');
+			sum %= divisor;
+			digits--;
+		}
+	}
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0
@@ -16,10 +56,7 @@ int main(void)
 			sum += i;
 	}
 
-	_putchar(sum / 1000 + '0');
-	_putchar((sum / 100) % 10 + '0');
-	_putchar((sum / 10) % 10 + '0');
-	_putchar(sum % 10 + '0');
+	print_sum(sum);
 	_putchar('\n');
 
 	return (0);
