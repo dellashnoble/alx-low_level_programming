ce4755dd9fffa730a81cc62dabaf30857e56346f
Otaru Favour <ashfabotaru@gmail.com>
2023-06-23 14:41:08 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square of size 'size' using the character '#'.
+ * @size: Size of the square.
+ *
+ * Description: Prints a square of size 'size' using the character '#'.
+ * If 'size' is 0 or less, prints only a newline.
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
+
