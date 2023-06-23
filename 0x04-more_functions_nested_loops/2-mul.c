826d84c5b2953f8521f860843e2a06f283084bec
Otaru Favour <ashfabotaru@gmail.com>
2023-06-23 13:56:33 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * multiply - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of the two integers.
+ */
+int multiply(int a, int b)
+{
+	return (a * b);
+}
+
