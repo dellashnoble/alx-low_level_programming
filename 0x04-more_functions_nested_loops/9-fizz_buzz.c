7616a853349291eeca6a3c50dcd96593360b8854
Otaru Favour <ashfabotaru@gmail.com>
2023-06-23 14:49:31 -0700

    task8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the numbers from 1 to 100 with certain conditions.
+ * Multiples of three are replaced with "Fizz", multiples of five are replaced
+ * with "Buzz", and multiples of both 3 and 5 are replaced with "FizzBuzz".
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
