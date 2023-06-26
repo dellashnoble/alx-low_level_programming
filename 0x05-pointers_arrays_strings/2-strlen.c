4b4a39d024f608f1f21b11f8a7ddb681f381a68f
Otaru Favour <ashfabotaru@gmail.com>
2023-06-26 11:05:19 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * string_length - Computes the length of a string.
@@ -7,11 +6,17 @@
  *
  * Return: The length of the string.
  *
- * Description: This function takes a string as a parameter and uses the
- * strlen function from the standard library to compute the length of the string.
+ * Description: This function takes a string as a parameter and computes
+ * the length of the string by iterating through it until the null character
+ * ('\0') is encountered.
  */
 int string_length(const char *str)
 {
-    return strlen(str);
+	int length = 0;
+
+	while (str[length] != '\0')
+		length++;
+
+	return length;
 }
 
