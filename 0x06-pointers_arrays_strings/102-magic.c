fbbe3b5fc6a6d1d30f4f92dcf73ca52d11f3d634
Otaru Favour <ashfabotaru@gmail.com>
2023-06-29 13:04:47 -0700

    task91011

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A program to demonstrate magic
+ * magic is performed to assign 98 to a[2]
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -8,11 +16,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
-	/* write your line of code here... */
-	*(&n + 12) = 98; /* Address arithmetic: n + 12 = &a[2] */
-	/*******************************/
-
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
