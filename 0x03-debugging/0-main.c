154423a3beab63ede6778f682b91e96861109850
Otaru Favour <ashfabotaru@gmail.com>
2023-07-02 00:24:24 -0700

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program
- *
- * This function tests the positive_or_negative function.
- * It sets the variable 'i' to 0 and calls the function.
- *
- * Return: Always 0 (success)
- */
+* main - Test function for positive or negative
+* It sets the variable 'i' to 0 and calls the function.
+*
+* Return: 0
+*/
 int main(void)
 {
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
 
