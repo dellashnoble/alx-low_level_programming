b2e35340d18fb0fbf6658818133d87ed0a9fc43d
Otaru Favour <ashfabotaru@gmail.com>
2023-07-02 00:32:13 -0700

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+
 /**
-* main - causes an infinite loop
+* main - Entry point of the program
+*
+* This function causes an infinite loop but then avoids it.
+*
 * Return: 0
 */
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
 i = 0;
+
 /*while (i < 10)*/
 /*{*/
 /*putchar(i);*/
