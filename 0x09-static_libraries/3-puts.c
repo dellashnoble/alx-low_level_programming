2af75ae257cfcfc87dbc65981eea28c5b6afa307
Otaru Favour <ashfabotaru@gmail.com>
2023-07-07 14:18:01 -0700

    task1

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: input string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	/* Function implementation */
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
